11c7c77d0fcf81815e37b70bb1bbabd9b826a615
ikromr04 <ikromr04@gmail.com>
2024-03-15 08:53:46 +0500

    Project init

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+  use HasFactory, Sluggable;
+
+  protected $guarded = [];
+
+  public function sluggable(): array
+  {
+    return [
+      'slug' => [
+        'source' => 'ru_title'
+      ]
+    ];
+  }
+
+  public function category()
+  {
+    return $this->belongsTo(ProductsCategory::class, 'category_id');
+  }
+}
